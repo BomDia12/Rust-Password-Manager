02c652e875ca8d905ec5c5043b8f5ddd3b8c5ae2
iIiqwertyiIi <nicpabe@gmail.com>
2023-12-12 10:22:23 -0300

    added documentation to types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,13 @@
+/// Importando da biblioteca `serde` as funções de serialização e desserialização.
 use serde::{Serialize, Deserialize};
+/// Carregando os macros de serialização, desserialização e debug.
 #[derive(Serialize, Deserialize, Debug)]
+
+/// Implementação da estrutura de entrada, que terá:
+/// # Atributos
+/// * `username` - uma string que contém o nome de usuário da conta a ser guardada
+/// * `domain` - domínio do site ao qual a conta pertence
+/// * `password` - senha da conta guardada
 pub struct Entry {
     pub username: String,
     pub domain: String,

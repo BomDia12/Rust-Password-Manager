2d78c118b58d41025ccb23eabe64e075ba5d8f0b
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2023-12-12 20:24:49 -0300

    build: aes (not working)

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,9 +1,27 @@
-pub fn encrypt_data(data: String) -> String {
-    data
+use sha2::{Sha256, Digest};
+use aes::Aes256;
+use block_modes::{BlockMode, Cfb};
+use block_modes::block_padding::Pkcs7;
+use hex_literal::hex;
+use std::str;
+use std::env;
+pub fn encrypt_data(data: String, key) {
+    let cipher = Aes256::new(&key);
+    let plaintext = data.as_bytes();
+    let plainTextSize = plaintext.len();
+    let mut buffer = [0u8; 256];
+    buffer[..plainTextSize].copy_from_slice(plaintext)
+    let ciphertext = cipher.encrypt(&mut buffer, plainTextSize).unwrap();
+    hex::encode(ciphertext)
 }
 
-pub fn decrypt_data(data: Vec<u8>) -> String {
-    String::from_utf8(data).expect("não converteu")
+pub fn decrypt_data(ciphertext, key) -> String {
+    let cipher = Aes256::new(&key);
+    let mut buf = ciphertext.to_vec();
+    let decrypted = cipher.decrypt(&mut buf).unwrap();
+    str::from_utf8(decrypted).unwrap()
 }
 
-pub fn generate_encryption_key() {}
+pub fn generate_encryption_key<Sha256: Digest>(input_string: String) {
+    let hash = Sha256::digest(input_string.as_bytes());
+}   

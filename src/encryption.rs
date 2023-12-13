68ffd13f7044adc8480882f24ab0af75f29957c1
Arthur Mota Furtado <arthur.mota@syngenta.com>
2023-12-12 22:11:11 -0300

    No check errors

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,27 +1,61 @@
-use sha2::{Sha256, Digest};
-use aes::Aes256;
-use block_modes::{BlockMode, Cfb};
-use block_modes::block_padding::Pkcs7;
-use hex_literal::hex;
-use std::str;
-use std::env;
-pub fn encrypt_data(data: String, key) {
-    let cipher = Aes256::new(&key);
-    let plaintext = data.as_bytes();
-    let plainTextSize = plaintext.len();
-    let mut buffer = [0u8; 256];
-    buffer[..plainTextSize].copy_from_slice(plaintext)
-    let ciphertext = cipher.encrypt(&mut buffer, plainTextSize).unwrap();
-    hex::encode(ciphertext)
+use openssl::{aes::{AesKey, aes_ige}, sha::Sha256, symm::Mode};
+
+pub fn encrypt_data(data: String, key: &AesKey) -> String {
+    let blocks = break_data(data.into_bytes());
+    let mut res: Vec<u8> = Vec::new();
+    let mut iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
+                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
+    for block in blocks {
+        let mut tmp_res = [0u8; 16];
+        aes_ige(&block, &mut tmp_res, key, &mut iv, Mode::Encrypt);
+        for byte in tmp_res {
+            res.push(byte);
+        }
+    }
+    String::from_utf8(res).unwrap()
+} 
+
+pub fn decrypt_data(data: Vec<u8>, key: &AesKey) -> String {
+    let blocks = break_data(data);
+    let mut res: Vec<u8> = Vec::new();
+    let mut iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
+                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
+    for block in blocks {
+        let mut tmp_res = [0u8; 16];
+        aes_ige(&block, &mut tmp_res, key, &mut iv, Mode::Decrypt);
+        for byte in tmp_res {
+            res.push(byte);
+        }
+    }
+    String::from_utf8(res).unwrap()
 }
 
-pub fn decrypt_data(ciphertext, key) -> String {
-    let cipher = Aes256::new(&key);
-    let mut buf = ciphertext.to_vec();
-    let decrypted = cipher.decrypt(&mut buf).unwrap();
-    str::from_utf8(decrypted).unwrap()
+fn break_data(data: Vec<u8>) -> Vec<[u8; 16]> {
+    let mut res = Vec::new();
+    let mut count = 0;
+    let mut block = [0u8; 16];
+    for byte in data {
+        block[count] = byte;
+        count += 1;
+        if count == 8 {
+            count = 0;
+            res.push(block);
+        }
+    }
+    if count != 0 {
+        while count < 8 {
+            block[count] = 0;
+            count += 1;
+        }
+        res.push(block);
+    }
+    res
 }
 
-pub fn generate_encryption_key<Sha256: Digest>(input_string: String) {
-    let hash = Sha256::digest(input_string.as_bytes());
-}   
+pub fn generate_key(input_password: &str) -> AesKey {
+    let mut hash = Sha256::new();
+    hash.update(input_password.as_bytes());
+    let res = hash.finish();
+    let res = &res[0..16];
+    AesKey::new_encrypt(&res).unwrap()
+}
--- src/persistency.rs
+++ src/persistency.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 pub fn save_data_to_disk(data: &Vec<Entry>) {
     let json = serde_json::to_string(&data).expect("Erro serializer");
-    let encrypted_data = encrypt_data(json);
+    let encrypted_data = encrypt_data(json, "");
     fs::write("data", encrypted_data).expect("Erro escrevendo json");
 }
 
@@ -12,7 +12,7 @@ pub fn read_data_from_disk(data: &mut Vec<Entry>) {
         Ok(data) => data,
         Err(_) => return
     };
-    let decrypted_data = decrypt_data(encrypted_data);
+    let decrypted_data = decrypt_data(encrypted_data, "");
     let deserialized: Vec<Entry> = serde_json::from_str(&decrypted_data).expect("Erro deserialize");
     for i in deserialized {
         data.push(i)
